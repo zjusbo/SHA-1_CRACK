a598624f149cd49747a125a840ec74deea8d8353
songbo <sbo@zju.edu.cn>
2015-04-19 20:38:44 +0800

    fix bugs, back track to single thread implement

--- crack_sha-1.py
+++ crack_sha-1.py
@@ -2,15 +2,15 @@
 import hashlib
 import time
 import os
-filename = 'schedule.txt'
-resultname = 'result.txt'
+filename = 'schedule' #储存当前的破解进度
+resultname = 'result' #最终的破解结果
 order = 0
-cipher = 'd033e22ae348aeb5660fc2140aec35850c4da997'
-ciphertest = '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'
+#cipher = 'd033e22ae348aeb5660fc2140aec35850c4da997'  #要破解的SHA1密文
+cipher = '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'
 #dic = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-dic = ['0','1','2','3','4','5','6','7','8','9']
+dic = ['0','1','2','3','4','5','6','7','8','9'] #破解时使用的字典，此处为所有的0-9数字组合。上面一行是所有的字母数字组合，根据需要进行注释调整。
 
-def generateText(order):
+def generateText(order): #根据order从dic字典中生成尝试破解的明文
 	text = ''
 	if order == 0:
 		return '0'
@@ -29,7 +29,7 @@ def main():
 		run()
 	except:
 		pass
-	finally:
+	finally: #如果程序异常终止，如用户输入了ctrl+c中断，则将当前的破解进度存入"filename"文件中，以便之后从该处重启
 		if os.path.isfile(filename):
 			os.remove(filename)
 		fp = open(filename,'w')
@@ -46,7 +46,7 @@ def run():
 	while found == False:
 		text = str(order)
 		order += 1
-		if hashlib.sha1(text).hexdigest() == cipher:
+		if hashlib.sha1(text).hexdigest() == cipher: #此行为该破解算法的核心，检测明文"text"的SHA1哈希值是否与CIPHER相等
 			print text
 			print "find!"
 			if os.path.isfile(resultname):
@@ -55,11 +55,10 @@ def run():
 			fp.write(text)
 			fp.close()
 			found = True
-		if order % 10000 == 0:
-			if time.time() - thistime > 3:
-				print '[%s items per second] current check text: %s ' % (((order - thisorder) // (time.time() - thistime)), text)
-				thistime = time.time()
-				thisorder = order
+		if time.time() - thistime > 1: 
+		        print '[%s items per second] currently trying: %s ' % (((order - thisorder) // (time.time() - thistime)), text)
+			thistime = time.time()
+			thisorder = order
 
 
 if __name__ == '__main__':
